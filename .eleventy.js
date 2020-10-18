44528ffad3e5c6db1a36e4da826eaf435a69ce27
Ross Phillips <rphillips-nz@users.noreply.github.com>
2020-10-18 20:28:44 +1300

    Add SEO plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 const pluginSass = require('eleventy-plugin-sass');
+const pluginSeo = require('eleventy-plugin-seo');
+const seoData = require('./_data/seo.json');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('images');
 
 	eleventyConfig.addPlugin(pluginSass, {
-		 watch: ['**/*.{scss,sass}', '!node_modules/**'],
+		watch: ['**/*.{scss,sass}', '!node_modules/**'],
 	});
+
+	eleventyConfig.addPlugin(pluginSeo, seoData);
 
 	eleventyConfig.addFilter('removeUrlProtocol', function (value) {
 		return value.replace(/^[a-z]*:?\/\//i, '');
